41d8a5f01c0c391360a8e8d4a938bf0646011a43
lanou3g <lanou3g@lanou3gdeMac-mini-5.local>
2015-10-06 14:46:54 +0800

    [add] add xcode code

---
+++ project_A/project_ATests/project_ATests.m
@@ -0,0 +1,40 @@
+//
+//  project_ATests.m
+//  project_ATests
+//
+//  Created by lanou3g on 15/10/6.
+//  Copyright (c) 2015年 蓝鸥科技. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface project_ATests : XCTestCase
+
+@end
+
+@implementation project_ATests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
